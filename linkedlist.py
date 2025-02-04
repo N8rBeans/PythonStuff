bd26ae39bab0310f864c1451b928ecbe149ceb6a
N8rBeans <github@nathanj.us>
2025-02-04 11:20:00 -0800

    update

--- linkedlist.py
+++ linkedlist.py
@@ -31,6 +31,20 @@ class LinkedList:
         self.length += 1
         return True
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if(self.length == 0):
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
 myLinkedList = LinkedList(5)
 myLinkedList.append(6)
+myLinkedList.prepend(7)
+myLinkedList.prepend(8)
+myLinkedList.append(9)
 myLinkedList.print_list()
